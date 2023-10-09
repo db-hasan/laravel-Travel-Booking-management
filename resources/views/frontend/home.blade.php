cbd9585caf559cd04d413d5499404a2c5190ccba
db-hasan <infoalihasanbd@gmail.com>
2023-10-09 12:20:53 +0600

    updt

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('frontend/css/style.css') }} ">
+    <link rel="stylesheet" href="{{asset('bootstrap/bootstrap.min.css') }} ">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <title>Document</title>
 </head>
 
 <body>
-    <h1 class="test">This is Home Page</h1>
+    <h1 class="">This is Home Page</h1>
+
+    <script src="{{asset('bootstrap/bootstrap.bundle.min.js') }} "></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('frontend/home');
 });
 
-Route::get('/admin', function () {
+Route::get('/', function () {
     return view('backend/dashboard');
 });
