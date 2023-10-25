2968a0ade31e6b9f786089e38df1444ba34e200b
db-hasan <infoalihasanbd@gmail.com>
2023-10-26 00:00:23 +0600

    updt

--- resources/views/backend/package/show.blade.php
+++ resources/views/backend/package/show.blade.php
@@ -1,7 +1,7 @@
 @extends('backend/layouts/layout')
 
 @section('content')
-<div class="container p-5">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
